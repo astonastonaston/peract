5c7924266bf1240cce650de3e6c20c990b37f763
astonastonaston <naxiao@ucsd.edu>
2024-09-12 15:38:01 -0700

    added pod supports

---
+++ scripts/prepare_env_and_dataset.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/astonastonaston/peract.git && cd peract && git checkout ms3
+
+conda install nvidia/label/cuda-12.1.0::cuda-toolkit && pip install "git+https://github.com/facebookresearch/pytorch3d.git"
+
+pip install -r requirements.txt
+
+sh scripts/ms3_demo_download.sh
