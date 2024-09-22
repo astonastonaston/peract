0c17b9118d09fedf1fe26550cb984430bbab34f3
astonastonaston <119010344@link.cuhk.edu.cn>
2024-09-21 17:37:00 -0700

    multi-view replay tools added

--- scripts/ms3_demo_download.sh
+++ scripts/ms3_demo_download.sh
@@ -3,6 +3,7 @@ mkdir demos
 python -m mani_skill.utils.download_demo "PushCube-v1" -o "demos"
 
 # Replay the trajectory to get pointcloud observations (default in pd_joint_pos control mode)
-python -m mani_skill.trajectory.replay_trajectory --traj-path demos/PushCube-v1/motionplanning/trajectory.h5 --save-traj --obs-mode pointcloud --num-procs 1 --count 100
+python -m replay_tools.replay_trajectory --traj-path demos/PushCube-v1/motionplanning/trajectory.h5 --save-traj --obs-mode pointcloud --num-procs 1 --count 100
+# python -m mani_skill.trajectory.replay_trajectory --traj-path demos/PushCube-v1/motionplanning/trajectory.h5 --save-traj --obs-mode pointcloud --num-procs 1 --count 100
 # python -m mani_skill.trajectory.replay_trajectory --traj-path demos/PushCube-v1/motionplanning/trajectory.h5 --save-traj --obs-mode rgbd --num-procs 1 --count 200
 # python -m mani_skill.trajectory.replay_trajectory --traj-path demos/PushCube-v1/motionplanning/trajectory.h5 --save-traj --obs-mode pointcloud --num-procs 1 --count 200
