aad082e508e0348351d3ed4f0bb74dcf0191ce93
astonastonaston <119010344@link.cuhk.edu.cn>
2024-09-26 11:13:49 -0700

    debugging

--- scripts/save_weights.sh
+++ scripts/save_weights.sh
@@ -1,3 +1,5 @@
 # save results to permanent storage
-cp -r /tmp/arm_test /dev/nanxiao-vol1/
-cp -r /tmp/arm /dev/nanxiao-vol1/
+mkdir /dev/nanxiao-vol1/arm_test_kp_red_cam_2
+mkdir /dev/nanxiao-vol1/arm_kp_red_cam_2
+cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_kp_red_cam_2
+cp -r /tmp/arm /dev/nanxiao-vol1/arm_kp_red_cam_2
