9a38cd3de462b61b61169b779dd6caa256ea9e39
astonastonaston <119010344@link.cuhk.edu.cn>
2024-09-27 00:33:44 -0700

    debugging

--- scripts/load_weights.sh
+++ scripts/load_weights.sh
@@ -1,3 +1,3 @@
 # load weights from permanent storage to the runtime directory
-cp -r /dev/nanxiao-vol1/arm_test_kp_red_cam_2/arm_test /tmp/
-cp -r /dev/nanxiao-vol1/arm_kp_red_cam_2/arm /tmp/
+cp -r /dev/nanxiao-vol1/arm_test_kp_red_cam_3/arm_test /tmp/
+cp -r /dev/nanxiao-vol1/arm_kp_red_cam_3/arm /tmp/
--- scripts/save_weights.sh
+++ scripts/save_weights.sh
@@ -1,5 +1,5 @@
 # save results to permanent storage
-mkdir /dev/nanxiao-vol1/arm_test_kp_red_cam_2
-mkdir /dev/nanxiao-vol1/arm_kp_red_cam_2
-cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_kp_red_cam_2
-cp -r /tmp/arm /dev/nanxiao-vol1/arm_kp_red_cam_2
+mkdir /dev/nanxiao-vol1/arm_test_kp_red_cam_3
+mkdir /dev/nanxiao-vol1/arm_kp_red_cam_3
+cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_kp_red_cam_3
+cp -r /tmp/arm /dev/nanxiao-vol1/arm_kp_red_cam_3
