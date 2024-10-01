8615c10efe00516fe2ec3de9f35e36c781d783d4
astonastonaston <119010344@link.cuhk.edu.cn>
2024-10-01 10:38:18 -0700

    debug

--- scripts/load_weights.sh
+++ scripts/load_weights.sh
@@ -1,6 +1,6 @@
 # load weights from permanent storage to the runtime directory
-cp -r /dev/nanxiao-vol1/arm_test_bnd/arm_test /tmp/
-cp -r /dev/nanxiao-vol1/arm_bnd/arm /tmp/
+cp -r /dev/nanxiao-vol1/arm_test_kp_red_cam_3_aug5/arm_test /tmp/
+cp -r /dev/nanxiao-vol1/arm_kp_red_cam_3_aug5/arm /tmp/
 
 # # load weights from permanent storage to the runtime directory
 # cp -r /dev/nanxiao-vol1/arm_test_kp_red_cam_3/arm_test /tmp/
--- scripts/save_weights.sh
+++ scripts/save_weights.sh
@@ -5,7 +5,7 @@
 # cp -r /tmp/arm /dev/nanxiao-vol1/arm_kp_red_cam_3
 
 # save results to permanent storage
-mkdir /dev/nanxiao-vol1/arm_test_bnd
-mkdir /dev/nanxiao-vol1/arm_bnd
-cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_bnd
-cp -r /tmp/arm /dev/nanxiao-vol1/arm_bnd
+mkdir /dev/nanxiao-vol1/arm_test_kp_red_cam_3_aug5
+mkdir /dev/nanxiao-vol1/arm_kp_red_cam_3_aug5
+cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_kp_red_cam_3_aug5
+cp -r /tmp/arm /dev/nanxiao-vol1/arm_kp_red_cam_3_aug5
