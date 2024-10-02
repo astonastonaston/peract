407363aee49e5b9c852f45a2d07ea73f6f882c64
astonastonaston <119010344@link.cuhk.edu.cn>
2024-10-02 00:24:40 -0700

    debug demo 30

--- scripts/load_weights.sh
+++ scripts/load_weights.sh
@@ -1,6 +1,9 @@
 # load weights from permanent storage to the runtime directory
-cp -r /dev/nanxiao-vol1/arm_test_bnd/arm_test /tmp/
-cp -r /dev/nanxiao-vol1/arm_bnd/arm /tmp/
+cp -r /dev/nanxiao-vol1/arm_test_bnd_30/arm_test /tmp/
+cp -r /dev/nanxiao-vol1/arm_bnd_30/arm /tmp/
+
+# cp -r /dev/nanxiao-vol1/arm_test_bnd/arm_test /tmp/
+# cp -r /dev/nanxiao-vol1/arm_bnd/arm /tmp/
 
 # # load weights from permanent storage to the runtime directory
 # cp -r /dev/nanxiao-vol1/arm_test_kp_red_cam_3/arm_test /tmp/
--- scripts/save_weights.sh
+++ scripts/save_weights.sh
@@ -5,10 +5,16 @@
 # cp -r /tmp/arm /dev/nanxiao-vol1/arm_kp_red_cam_3
 
 # save results to permanent storage
-mkdir /dev/nanxiao-vol1/arm_test_bnd
-mkdir /dev/nanxiao-vol1/arm_bnd
-cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_bnd
-cp -r /tmp/arm /dev/nanxiao-vol1/arm_bnd
+mkdir /dev/nanxiao-vol1/arm_test_bnd_30
+mkdir /dev/nanxiao-vol1/arm_bnd_30
+cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_bnd_30
+cp -r /tmp/arm /dev/nanxiao-vol1/arm_bnd_30
+
+# # save results to permanent storage
+# mkdir /dev/nanxiao-vol1/arm_test_bnd
+# mkdir /dev/nanxiao-vol1/arm_bnd
+# cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_bnd
+# cp -r /tmp/arm /dev/nanxiao-vol1/arm_bnd
 
 # mkdir /dev/nanxiao-vol1/arm_test_kp_red_cam_3_aug5
 # mkdir /dev/nanxiao-vol1/arm_kp_red_cam_3_aug5
