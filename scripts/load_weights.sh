4ecc591af7b7a32ab8ce42119c4ac7520f69e3b4
astonastonaston <119010344@link.cuhk.edu.cn>
2024-10-02 20:39:40 -0700

    desc debug

--- scripts/load_weights.sh
+++ scripts/load_weights.sh
@@ -1,6 +1,6 @@
 # load weights from permanent storage to the runtime directory
-cp -r /dev/nanxiao-vol1/arm_test_bnd_ts_10_6/arm_test /tmp/
-cp -r /dev/nanxiao-vol1/arm_bnd_ts_10_6/arm /tmp/
+cp -r /dev/nanxiao-vol1/arm_test_bnd_ts_desc/arm_test /tmp/
+cp -r /dev/nanxiao-vol1/arm_bnd_ts_desc/arm /tmp/
 
 # # load weights from permanent storage to the runtime directory
 # cp -r /dev/nanxiao-vol1/arm_test_bnd_30/arm_test /tmp/
