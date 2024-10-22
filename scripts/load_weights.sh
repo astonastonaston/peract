f6812a5e1450bdb09cf4f465274d308f89a8b1e1
astonastonaston <119010344@link.cuhk.edu.cn>
2024-10-22 04:25:57 -0700

    demo unified with long desc trial

--- scripts/load_weights.sh
+++ scripts/load_weights.sh
@@ -1,6 +1,6 @@
 # load weights from permanent storage to the runtime directory
-cp -r /dev/nanxiao-vol1/arm_test_unified_demos/arm_test /tmp/
-cp -r /dev/nanxiao-vol1/arm_unified_demos/arm /tmp/
+cp -r /dev/nanxiao-vol1/arm_test_unified_demos_longdesc/arm_test /tmp/
+cp -r /dev/nanxiao-vol1/arm_unified_demos_longdesc/arm /tmp/
 
 # # load weights from permanent storage to the runtime directory
 # cp -r /dev/nanxiao-vol1/arm_test_bnd_30/arm_test /tmp/
