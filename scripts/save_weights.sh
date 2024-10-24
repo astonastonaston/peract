59ef6e01c04c4542c610537685ca66ddfeafa0e1
astonastonaston <119010344@link.cuhk.edu.cn>
2024-10-24 04:44:19 -0700

    single cam 30 demos training with 60000 steps

--- scripts/save_weights.sh
+++ scripts/save_weights.sh
@@ -11,10 +11,10 @@
 # cp -r /tmp/arm /dev/nanxiao-vol1/arm_unified_demos_30_multi_cam
 
 # save results to permanent storage
-mkdir /dev/nanxiao-vol1/arm_test_unified_demos_50_single_cam
-mkdir /dev/nanxiao-vol1/arm_unified_demos_50_single_cam
-cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_unified_demos_50_single_cam
-cp -r /tmp/arm /dev/nanxiao-vol1/arm_unified_demos_50_single_cam
+mkdir /dev/nanxiao-vol1/arm_test_unified_demos_30_single_cam
+mkdir /dev/nanxiao-vol1/arm_unified_demos_30_single_cam
+cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_unified_demos_30_single_cam
+cp -r /tmp/arm /dev/nanxiao-vol1/arm_unified_demos_30_single_cam
 
 # # save results to permanent storage
 # mkdir /dev/nanxiao-vol1/arm_test_bnd_30
