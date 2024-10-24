f3205d225a24635129d09cfb6238648aebdfb44c
astonastonaston <119010344@link.cuhk.edu.cn>
2024-10-24 04:32:29 -0700

    multi cam 30 demos training

--- scripts/save_weights.sh
+++ scripts/save_weights.sh
@@ -11,6 +11,12 @@ cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_unified_demos_30_multi_cam
 cp -r /tmp/arm /dev/nanxiao-vol1/arm_unified_demos_30_multi_cam
 
 # # save results to permanent storage
+# mkdir /dev/nanxiao-vol1/arm_test_unified_demos_30_single_cam
+# mkdir /dev/nanxiao-vol1/arm_unified_demos_30_single_cam
+# cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_unified_demos_30_single_cam
+# cp -r /tmp/arm /dev/nanxiao-vol1/arm_unified_demos_30_single_cam
+
+# # save results to permanent storage
 # mkdir /dev/nanxiao-vol1/arm_test_bnd_30
 # mkdir /dev/nanxiao-vol1/arm_bnd_30
 # cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_bnd_30
