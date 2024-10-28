4a354b770a6dd139c6955d6ec08006046bf2da0e
astonastonaston <119010344@link.cuhk.edu.cn>
2024-10-28 03:26:36 -0700

    stopping delta = 0.1 experiment

--- scripts/save_weights.sh
+++ scripts/save_weights.sh
@@ -11,10 +11,10 @@
 # cp -r /tmp/arm /dev/nanxiao-vol1/arm_unified_demos_30_multi_cam
 
 # save results to permanent storage
-mkdir /dev/nanxiao-vol1/arm_test_unified_demos_pickcube_single_view_60_60ksteps_stopadj_dlta015
-mkdir /dev/nanxiao-vol1/arm_unified_demos_pickcube_single_view_60_60ksteps_stopadj_dlta015
-cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_unified_demos_pickcube_single_view_60_60ksteps_stopadj_dlta015
-cp -r /tmp/arm /dev/nanxiao-vol1/arm_unified_demos_pickcube_single_view_60_60ksteps_stopadj_dlta015
+mkdir /dev/nanxiao-vol1/arm_test_unified_demos_pickcube_single_view_60_60ksteps_stopadj_dlta01
+mkdir /dev/nanxiao-vol1/arm_unified_demos_pickcube_single_view_60_60ksteps_stopadj_dlta01
+cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_unified_demos_pickcube_single_view_60_60ksteps_stopadj_dlta01
+cp -r /tmp/arm /dev/nanxiao-vol1/arm_unified_demos_pickcube_single_view_60_60ksteps_stopadj_dlta01
 
 # # save results to permanent storage
 # mkdir /dev/nanxiao-vol1/arm_test_unified_demos_30_single_cam
