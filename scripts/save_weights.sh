3a752eb5b12ec2cf41c4fd54b512fd648411781c
astonastonaston <119010344@link.cuhk.edu.cn>
2024-10-29 00:50:55 -0700

    stopinit 6 experiment

--- scripts/save_weights.sh
+++ scripts/save_weights.sh
@@ -11,10 +11,10 @@
 # cp -r /tmp/arm /dev/nanxiao-vol1/arm_unified_demos_30_multi_cam
 
 # save results to permanent storage
-mkdir /dev/nanxiao-vol1/arm_test_unified_demos_stackcube_stopinit10
-mkdir /dev/nanxiao-vol1/arm_unified_demos_stackcube_stopinit10
-cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_unified_demos_stackcube_stopinit10
-cp -r /tmp/arm /dev/nanxiao-vol1/arm_unified_demos_stackcube_stopinit10
+mkdir /dev/nanxiao-vol1/arm_test_unified_demos_stackcube_stopinit6
+mkdir /dev/nanxiao-vol1/arm_unified_demos_stackcube_stopinit6
+cp -r /tmp/arm_test /dev/nanxiao-vol1/arm_test_unified_demos_stackcube_stopinit6
+cp -r /tmp/arm /dev/nanxiao-vol1/arm_unified_demos_stackcube_stopinit6
 
 # # save results to permanent storage
 # mkdir /dev/nanxiao-vol1/arm_test_unified_demos_30_single_cam
