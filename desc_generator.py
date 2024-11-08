d2b8bab99e8a30225b74698ac74c7e89d3fb64f8
astonastonaston <119010344@link.cuhk.edu.cn>
2024-11-07 16:57:02 -0800

    lang back

--- desc_generator.py
+++ desc_generator.py
@@ -3,7 +3,8 @@ import pickle
 # desc = ["close the gripper",
 #         "reach the cube on the table by moving the gripper to the back of the cube",
 #         "push the cube to a red white goal region in front of it"]
-desc = ["pick up a red cube, and then stack it on top of a green cube, and then let go of the red cube without it falling."]
+desc = ["pick up a red cube and stack it on top of a green cube and let go of the cube without it falling."]
+# desc = ["pick up a red cube, and then stack it on top of a green cube, and then let go of the red cube without it falling."]
 # desc = ["grasp the red cube and move it to the target goal position."]
 # desc = ["push the cube to the red white target on the table"]
 # desc = ["push the cube on the table to the center of the red white target on the table"]
