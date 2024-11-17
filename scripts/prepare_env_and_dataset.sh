3ef8a5e98d30e6fae564405cd6213ccb72d24a35
astonastonaston <119010344@link.cuhk.edu.cn>
2024-11-17 15:00:46 -0800

    conda debug

--- scripts/prepare_env_and_dataset.sh
+++ scripts/prepare_env_and_dataset.sh
@@ -1,8 +1,8 @@
 # git clone https://github.com/astonastonaston/peract.git && cd peract && git checkout nau
 
 # activate peract env
-# conda create -n "peract" "python==3.10" --yes
-# conda activate peract
+conda create -n "peract" "python==3.10" --yes
+conda activate peract
 pip install --upgrade pip
 
 # install mani_skill
