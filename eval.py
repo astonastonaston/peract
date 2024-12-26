2bdb5bd6be619c541cfa3111de1bee830a0987c0
astonastonaston <119010344@link.cuhk.edu.cn>
2024-12-26 04:42:30 -0800

    no multi-processing since wandb will have problem

--- eval.py
+++ eval.py
@@ -177,25 +177,35 @@ def eval_seed(train_cfg,
     # NOTE: in multi-task settings, each task is evaluated serially, which makes everything slow!
     split_n = utils.split_list(num_weights_to_eval, eval_cfg.framework.eval_envs)
     for split in split_n:
-        processes = []
-        print(f"The number of processes {len(split), sapien.Device('cuda')}")
+        # processes = []
+        # print(f"The number of processes {len(split), sapien.Device('cuda')}")
         for e_idx, weight_idx in enumerate(split):
             weight = weight_folders[weight_idx]
-            # TODO: the maniskill gym env is already parallalized, so don't need to rewrite torch multi-processing again
-            p = Process(target=env_runner.start,
-                        args=(weight,
-                              save_load_lock,
-                              writer_lock,
-                              env_config,
-                              train_config,
-                              e_idx % torch.cuda.device_count(),
-                              eval_cfg,
-                              train_cfg,
-                              wandb_run))
-            p.start()
-            processes.append(p)
-        for p in processes:
-            p.join()
+            env_runner.start(weight,
+                            save_load_lock,
+                            writer_lock,
+                            env_config,
+                            train_config,
+                            e_idx % torch.cuda.device_count(),
+                            eval_cfg,
+                            train_cfg,
+                            wandb_run)
+
+        #     # TODO: the maniskill gym env is already parallalized, so don't need to rewrite torch multi-processing again
+        #     p = Process(target=env_runner.start,
+        #                 args=(weight,
+        #                       save_load_lock,
+        #                       writer_lock,
+        #                       env_config,
+        #                       train_config,
+        #                       e_idx % torch.cuda.device_count(),
+        #                       eval_cfg,
+        #                       train_cfg,
+        #                       wandb_run))
+        #     p.start()
+        #     processes.append(p)
+        # for p in processes:
+        #     p.join()
 
     del env_runner
     del agent
