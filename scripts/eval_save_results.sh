e20c4eb43ad8610daca40dd2464204542843c024
astonastonaston <119010344@link.cuhk.edu.cn>
2025-01-12 02:39:51 -0800

    stackcube eval experiment

--- scripts/eval_save_results.sh
+++ scripts/eval_save_results.sh
@@ -1,3 +1,3 @@
-cp ckpts/multi/PERACT_BC/seed0/eval_data.csv /dev/nanxiao-vol1/eval_data_pushcube_se3_smaller_mv.csv
-# cp ckpts/multi/PERACT_BC/seed0/eval_data.csv /dev/nanxiao-vol1/eval_data_stackcube_se3_smaller_mv.csv
+# cp ckpts/multi/PERACT_BC/seed0/eval_data.csv /dev/nanxiao-vol1/eval_data_pushcube_se3_smaller_mv.csv
+cp ckpts/multi/PERACT_BC/seed0/eval_data.csv /dev/nanxiao-vol1/eval_data_stackcube_se3_smaller_mv.csv
 # cp ckpts/multi/PERACT_BC/seed0/eval_data.csv /dev/nanxiao-vol1/eval_data_pushcube_50demos_80ksteps_wandb.csv
