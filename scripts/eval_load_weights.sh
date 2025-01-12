634cdfbc2e4b1b16cc790c7b3b923123c6a7b7fc
astonastonaston <119010344@link.cuhk.edu.cn>
2025-01-11 19:07:00 -0800

    pushcube mv eval experiment

--- scripts/eval_load_weights.sh
+++ scripts/eval_load_weights.sh
@@ -3,6 +3,7 @@ mkdir ckpts/multi
 mkdir ckpts/multi/PERACT_BC
 mkdir ckpts/multi/PERACT_BC/seed0
 
-cp -r /dev/nanxiao-vol1/arm_test_stackcube_se3_smaller/arm_test/multi/PERACT_BC/seed0/weights ckpts/multi/PERACT_BC/seed0/
+cp -r /dev/nanxiao-vol1/arm_test_pushcube_se3_smaller_mv/arm_test/multi/PERACT_BC/seed0/weights ckpts/multi/PERACT_BC/seed0/
+# cp -r /dev/nanxiao-vol1/arm_test_stackcube_se3_smaller_mv/arm_test/multi/PERACT_BC/seed0/weights ckpts/multi/PERACT_BC/seed0/
 # cp -r /dev/nanxiao-vol1/arm_test_stackcube_50demos_80ksteps_wandb/arm_test/multi/PERACT_BC/seed0/weights ckpts/multi/PERACT_BC/seed0/
 # cp -r /dev/nanxiao-vol1/arm_test_pushcube_50demos_80ksteps_wandb/arm_test/multi/PERACT_BC/seed0/weights ckpts/multi/PERACT_BC/seed0/
