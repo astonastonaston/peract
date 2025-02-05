ac05f633bca253f95d168fa284986407ce61dde7
astonastonaston <nanxiao1@link.cuhk.edu.cn>
2025-02-05 15:01:01 -0800

    debug scripts

--- scripts/prepare_env_and_dataset.sh
+++ scripts/prepare_env_and_dataset.sh
@@ -1,5 +1,5 @@
 # git clone https://github.com/astonastonaston/peract.git && cd peract && git checkout nau && pip install wandb && wandb login 819698faf6bbdcededa5242175e2e29556a4bf89 
-. ./train_exp_name.sh
+. scripts/train_exp_name.sh
 
 # activate peract env
 pip install --upgrade pip
