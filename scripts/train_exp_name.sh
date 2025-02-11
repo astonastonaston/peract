541f2337c13de0cbe220eca02d193f08518399d8
astonastonaston <nanxiao1@link.cuhk.edu.cn>
2025-02-11 04:47:43 -0800

    peg mv2 12steps exp train

--- scripts/train_exp_name.sh
+++ scripts/train_exp_name.sh
@@ -1,3 +1,3 @@
-export expname=peginsertionside_nose3_mv2_stopinit8_steps15
+export expname=peginsertionside_nose3_mv2_stopinit8_steps12
 export taskname=PegInsertionSide-v1
 export configname=multi_cam_peginsertionside
