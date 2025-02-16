27c1a08fefffd536b23885533565582885967b9b
astonastonaston <nanxiao1@link.cuhk.edu.cn>
2025-02-15 19:06:18 -0800

    120k mv2 demo 50 step 15

--- scripts/train_exp_name.sh
+++ scripts/train_exp_name.sh
@@ -1,4 +1,4 @@
-export expname=peginsertionside_nose3_mv3_stopinit8_steps15_demo100
+export expname=peginsertionside_nose3_mv2_stopinit8_steps15
 export taskname=PegInsertionSide-v1
 export configname=multi_cam_peginsertionside
-export demo=100
+export demo=50
