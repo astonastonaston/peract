6edcfa090ff0f7d12b586f888210cf47f3a09f3f
astonastonaston <nanxiao1@link.cuhk.edu.cn>
2025-02-16 19:27:03 -0800

    120k mv3 demo 50 step 15 eval

--- scripts/eval_exp_name.sh
+++ scripts/eval_exp_name.sh
@@ -1,3 +1,3 @@
-export expname=peginsertionside_nose3_mv3_stopinit8_steps15_demo100
+export expname=peginsertionside_nose3_mv3_stopinit8_steps15
 export taskname=PegInsertionSide-v1
 export configname=multi_cam_peginsertionside
