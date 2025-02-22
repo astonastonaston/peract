02fe9bf43e5b2e2f647715f7d44e818438ddb13c
astonastonaston <nanxiao1@link.cuhk.edu.cn>
2025-02-22 00:19:44 -0800

    train with 500 demos exp

--- scripts/train_exp_name.sh
+++ scripts/train_exp_name.sh
@@ -1,4 +1,4 @@
-export expname=peginsertionside_nose3_mv2_demo300
+export expname=peginsertionside_nose3_mv2_demo500
 export taskname=PegInsertionSide-v1
 export configname=multi_cam_peginsertionside
-export demo=300
+export demo=500
