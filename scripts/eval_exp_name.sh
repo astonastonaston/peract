5915eed9069b30cac7dcaefd000061f87139980d
astonastonaston <nanxiao1@link.cuhk.edu.cn>
2025-03-10 16:00:43 -0700

    eval 30 steps

--- scripts/eval_exp_name.sh
+++ scripts/eval_exp_name.sh
@@ -1,3 +1,3 @@
-export expname=peginsertionside_nose3_mv2_demo300
+export expname=peginsertionside_nose3_mv2_30steps
 export taskname=PegInsertionSide-v1
 export configname=multi_cam_peginsertionside
